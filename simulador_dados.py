185176369eac21caffc0aff9e016c7b5650e0bd7
jisaiaslima35 <josepisaias@hotmail.com>
2023-07-02 01:30:29 -0300

    teste 2

--- simulador_dados.py
+++ simulador_dados.py
@@ -1,1 +1,22 @@
-print('ola mundo')
+import random
+
+class SimuladoDados:
+
+    def __init__(self):
+        self.numero_menor = 1
+        self.numero_maior = 6
+        self.teste = ''
+        self.mensagem = 'Rodar Mais uma Vez ? '
+
+    def Iniciar(self):
+        resposta = input(self.mensagem)
+        numero = random.randint(self.numero_menor,self.numero_maior)
+
+        if resposta == 's' or resposta == 'sim':
+            print('O NUMERO FOI ', numero)
+
+        else:
+            print('ate mais thauuu')
+            
+simulador = SimuladoDados()
+simulador.Iniciar()
