a4fafe29e9c191e8e505db1b1a7d90fa9c399723
jisaiaslima35 <josepisaias@hotmail.com>
2023-07-02 03:01:40 -0300

    salva  2

--- agenda_telefonica.py
+++ agenda_telefonica.py
@@ -1,16 +1,27 @@
 class AgendaTelefonica:
     def __init__(self):
-        self.opcao = input('''
-        ##########################
-        projeto agenda python
+        self.opcao = ''
 
-        Menu:
-        [1] cadastrar
-        [2] lista contato
+    def Exibir_menu(self):
+        print('###################################')
+        print('projeto agenda python')
+        print('Menu :')
+        print('''
+        [1] Cadastrar
+        [2] Excluir
         ''')
-
     def Iniciar(self):
-        inicio1 = self.opcao
+        while self.opcao not in ['1','2']:
+            self.Exibir_menu()
+            self.opcao = input('Escolha uma opçao Acima :')
+        
+        if self.opcao == '1':
+            self.Cadastrar()
+        else:
+            self.Excluir()
+    def Cadastrar(self):
+          print('vc escolheu a opçao de cadastro')
 
-agenda = AgendaTelefonica()
-agenda.Iniciar()
+    def Excluir(self):
+        print('vc vai exckuir')
+
