e9c2be4f41abbc1fabc93721ea11b5ef8b7dc6e3
jisaiaslima35 <josepisaias@hotmail.com>
2023-07-02 02:13:36 -0300

    agenda python 1

---
+++ agenda_telefonica.py
@@ -0,0 +1,16 @@
+class AgendaTelefonica:
+    def __init__(self):
+        self.opcao = input('''
+        ##########################
+        projeto agenda python
+
+        Menu:
+        [1] cadastrar
+        [2] lista contato
+        ''')
+
+    def Iniciar(self):
+        inicio1 = self.opcao()
+
+agenda = AgendaTelefonica()
+agenda.Iniciar()
