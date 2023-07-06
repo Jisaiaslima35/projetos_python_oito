8bff930b2858880d30d672238be8143c72d810af
jisaiaslima35 <josepisaias@hotmail.com>
2023-07-06 16:16:58 -0300

    vou acrecentar a funçao pesquisar pelo nome

--- agenda_telefonica.py
+++ agenda_telefonica.py
@@ -10,37 +10,66 @@ class AgendaTelefonica:
         print('projeto agenda python')
         print('Menu:')
         print('[1] Cadastrar')
-        print('[2] Excluir')
+        print('[2] Listar Conatos')
+        print('[3] Excluir')
+        print('[4] Buscar Pelo Nome')
 
     def Iniciar(self):
-        while self.opcao not in ['1', '2']:
+        while self.opcao not in ['1', '2', '3', '5']:
             self.Exibir_menu()
             self.opcao = input('Escolha uma opção acima: ')
 
         if self.opcao == '1':
             self.Cadastrar()
-        else:
+        elif self.opcao == '2':
+            self.Lista_contatos()
+        elif self.opcao == '3':
             self.Excluir()
+        elif self.opcao == '5':
+            self.Sair()
 
     def Cadastrar(self):
         idContato = self.id
-        nome = input('digite seu nome :')
-        telefone = input('digite seu telefone :')
-        email = input('digite seu email')
+        nome = input('Digite seu nome: ')
+        telefone = input('Digite seu telefone: ')
+        email = input('Digite seu email: ')
         # para criar e gravar no arquivo txt
         try:
-            agenda = open("agenda.txt","a")
+            agenda = open("agenda.txt", "a")
             dados = f'{idContato};{nome};{telefone},{email} \n'
             agenda.write(dados)
             agenda.close()
-            print(f'contatos salvo com sucesso')
+            print('Contato salvo com sucesso')
         except:
-            print('houve um ERRO')
-        
+            print('Houve um erro')
+
+    def Lista_contatos(self):
+        agenda = open("agenda.txt", "r")
+        for contato in agenda:
+            print(contato)
+        agenda.close()
+
+    def Limpar(self):
+        self.opcao = ''
+        self.id = random.randint(1, 10000)
 
     def Excluir(self):
         print(f'Você selecionou a opção de excluir o contato com ID {self.id}.')
 
+    def Sair(self):
+        print("Até mais")
+        exit()
+
+
 agenda = AgendaTelefonica()
 agenda.Iniciar()
 
+while True:
+    pergunta = input('Deseja continuar? [s] Sim | [n] Não ')
+    if pergunta.lower() == 's':
+        agenda.Limpar()
+        agenda.Iniciar()
+    elif pergunta.lower() == 'n':
+        break
+    else:
+        print("Escolha uma das opções abaixo")
